126e60f8c22fcc594782b49b51ecee973e74f290
Moustafa Zaki <moustafa.zaki@in3.cloud>
2020-02-02 18:56:25 +0200

    style(demo): make it PWA

--- projects/demo/src/main.ts
+++ projects/demo/src/main.ts
@@ -10,4 +10,11 @@ if (environment.production) {
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  // waiting for angular to load bootstrapping and then only loading service worker
+  .then(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register(`/ngsw-worker.js`);
+      console.log(`Registered as service worker`);
+    }
+  })
+  .catch(err => console.log(err));
