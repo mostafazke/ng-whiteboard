b45ac3b456697074d776508db6943c0179d7b8f9
Mostafa Zaki <mmostafazke@gmail.com>
2023-10-27 00:12:47 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/mostafazke/ng-whiteboard/commit/ba0df2a656f0dd998780975c41a0484dc128b00c
    CircleCI build: https://circleci.com/gh/mostafazke/ng-whiteboard/736

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -523,7 +523,12 @@ ${error.stack}`;
       await this.urls.reduce(async (previous, url) => {
         await previous;
         const req = this.adapter.newRequest(url);
-        const alreadyCached = await cache.match(req, this.config.cacheQueryOptions) !== void 0;
+        let alreadyCached = false;
+        try {
+          alreadyCached = await cache.match(req, this.config.cacheQueryOptions) !== void 0;
+        } catch (error) {
+          throw new SwCriticalError(`Cache is throwing while looking for a match in a PrefetchAssetGroup: ${error}`);
+        }
         if (alreadyCached) {
           return;
         }
@@ -560,7 +565,12 @@ ${error.stack}`;
       await this.urls.reduce(async (previous, url) => {
         await previous;
         const req = this.adapter.newRequest(url);
-        const alreadyCached = await cache.match(req, this.config.cacheQueryOptions) !== void 0;
+        let alreadyCached = false;
+        try {
+          alreadyCached = await cache.match(req, this.config.cacheQueryOptions) !== void 0;
+        } catch (error) {
+          throw new SwCriticalError(`Cache is throwing while looking for a match in a LazyAssetGroup: ${error}`);
+        }
         if (alreadyCached) {
           return;
         }
@@ -1022,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/darwin-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "16.2.7";
+  var SW_VERSION = "16.2.11";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
