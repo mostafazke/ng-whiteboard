b07f67b3c9d3c93e402de7f83f39e7d987e9b747
Mostafa Zaki <mmostafazke@gmail.com>
2026-06-25 17:24:59 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/mostafazke/ng-whiteboard/commit/05897e87396240983f080a2562d62ad49395a2f3
    GitHub Actions build: https://github.com/mostafazke/ng-whiteboard/actions/runs/28187885076

--- browser/chunk-G2ZET2WZ.js
+++ browser/chunk-G2ZET2WZ.js
@@ -1,4 +1,4 @@
-import{h as p}from"./chunk-R2XNNP2S.js";import{a as u}from"./chunk-UXZKHGXZ.js";import{b as d,e as s}from"./chunk-WXUOEA6O.js";import"./chunk-RGYB43VJ.js";import"./chunk-7EYOIDWW.js";import{Ma as r,Ob as t,Pb as c,bb as m,ub as n,vb as e,wb as l}from"./chunk-MHDMU4US.js";var M=(()=>{let o=class o{constructor(){this.libraryImportCode="import { NgWhiteboardComponent } from 'ng-whiteboard';",this.NgModuleCode=`@NgModule({
+import{h as p}from"./chunk-2RUT3425.js";import{a as u}from"./chunk-UXZKHGXZ.js";import{b as d,e as s}from"./chunk-WXUOEA6O.js";import"./chunk-RGYB43VJ.js";import"./chunk-7EYOIDWW.js";import{Ma as r,Ob as t,Pb as c,bb as m,ub as n,vb as e,wb as l}from"./chunk-MHDMU4US.js";var M=(()=>{let o=class o{constructor(){this.libraryImportCode="import { NgWhiteboardComponent } from 'ng-whiteboard';",this.NgModuleCode=`@NgModule({
   imports: [
     // Other imports
     NgWhiteboardComponent
